60ccdbb958375262c34518e5657fc98b290e3413
yurt <sherferyu@163.com>
2019-08-22 10:18:05 +0800

    editor over

--- example/route/editor-test/index.tsx
+++ example/route/editor-test/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import DD from '../../../src'
-import initCells from '../../config'
+import config from '../../config'
 import './index.scss'
 
 const { useState, useCallback } = React
@@ -8,8 +8,8 @@ const { Editor } = DD
 
 export default function () {
 
-  const [pageWidth, setPageWidth] = useState(360)
-  const [pageHeight, setPageHeight] = useState(640)
+  const [pageWidth, setPageWidth] = useState(1000)
+  const [pageHeight, setPageHeight] = useState(800)
 
   const handleChangeWidth = useCallback((event) => {
     const newValue = Number(event.target.value)
@@ -21,36 +21,15 @@ export default function () {
     setPageHeight(newValue || pageHeight)
   }, [])
 
-  const asyncCells = useCallback(() => {
-    return initCells
-  }, [])
-
   return (
     <div className="test">
       <div className="test-editor">
         <Editor
+          {...config}
           id="001"
-          cells={asyncCells()}
           width={pageWidth}
           height={pageHeight}
-          style={{ backgroundColor: '#ff8080' }}
-        />
-      </div>
-      <div
-        className="test-form"
-      >
-        <label>宽度：</label><input value={pageWidth} onChange={handleChangeWidth} type="text"/><br/>
-        <label>高度：</label><input value={pageHeight} onChange={handleChangeHeight} type="text"/><br/>
-        <button>复制当前选中的元素</button><br/>
-        <button>删除当前选中的元素</button><br/>
-      </div>
-      <div className="test-editor">
-        <Editor
-          id="002"
-          cells={asyncCells()}
-          width={pageWidth}
-          height={pageHeight}
-          style={{ backgroundColor: '#ff8080' }}
+          style={{ backgroundColor: '#f3f1ef' }}
         />
       </div>
     </div>
